b1d0990d7ec6e1c6e49f5e15c72993691e9d1f4f
Maxime Locqueville <maxiloc@gmail.com>
2016-02-12 11:18:01 +0100

    add algolia docsearch

--- themes/vue/source/js/common.js
+++ themes/vue/source/js/common.js
@@ -20,12 +20,11 @@
    */
 
   function initSearch () {
-    (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
-    (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
-    e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
-    })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
-
-    _st('install','HgpxvBc7pUaPUWmG9sgv','2.0.0');
+    docsearch({
+      apiKey: '0a75952972806d9ad07e387d08e9cc4c',
+      indexName: 'vuejs_jp',
+      inputSelector: '.st-default-search-input'
+    });
   }
 
   /**
