09cf27ab2b1037a966f2f63666db8c788b19d16d
re-fort <jeffpko@gmail.com>
2017-12-28 15:28:27 +0900

    fix fuzzy matching when match already exists
    ref: https://github.com/vuejs/vuejs.org/commit/85329db4229492692ff4df537004790a10143174

--- themes/vue/source/js/common.js
+++ themes/vue/source/js/common.js
@@ -43,24 +43,23 @@
     }
   }
 
-  function escapeCharacters (hash) {
+  function parseRawHash (hash) {
     // Remove leading hash
     if (hash.charAt(0) === '#') {
       hash = hash.substr(1)
     }
 
-    return '#' + CSS.escape(hash)
+    // Escape characthers
+    try {
+      hash = decodeURIComponent(hash)
+    } catch (e) {}
+    return CSS.escape(hash)
   }
 
   function initLocationHashFuzzyMatching () {
     var rawHash = window.location.hash
     if (!rawHash) return
-    var hash
-    try {
-      hash = escapeCharacters(decodeURIComponent(rawHash))
-    } catch(e) {
-      hash = escapeCharacters(rawHash)
-    }
+    var hash = parseRawHash(rawHash)
     var hashTarget = document.getElementById(hash)
     if (!hashTarget) {
       var normalizedHash = normalizeHash(hash)
@@ -73,7 +72,7 @@
         if (distanceA > distanceB) return 1
         return 0
       })
-      window.location.hash = encodeURIComponent(possibleHashes[0])
+      window.location.hash = '#' + possibleHashes[0]
     }
 
     function normalizeHash (rawHash) {
