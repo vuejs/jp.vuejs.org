da0e475a524764dad0c8ae6320e82c3a9a28b0af
Evan You <yyx990803@gmail.com>
2022-06-02 11:51:01 +0800

    fix build

--- _scripts/sync-sponsors.js
+++ _scripts/sync-sponsors.js
@@ -2,18 +2,17 @@
 const fs = require('fs')
 const path = require('path')
 const axios = require('axios')
-const yaml = require('yaml')
+const yaml = require('js-yaml')
 
 const configPath = path.resolve(__dirname, '../themes/vue/_config.yml')
 
 ;(async () => {
   const { data } = await axios(`https://sponsors.vuejs.org/data.json`)
-  const yml = yaml.stringify(data)
+  const yml = yaml.dump(data)
   const config = fs.readFileSync(configPath, 'utf-8')
-  const updated = config
-    .replace(/(# START SPONSORS)[^]*(# END SPONSORS)/, `$1\n${yml}$2`)
-    // jp specific logic
-    .replace(`https://code-dict.com`, `https://jp.code-dict.com`)
-
+  const updated = config.replace(
+    /(# START SPONSORS)[^]*(# END SPONSORS)/,
+    `$1\n${yml}$2`
+  )
   fs.writeFileSync(configPath, updated)
 })()
